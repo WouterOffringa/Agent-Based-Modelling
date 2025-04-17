e6a37e07edee95a8d8dccc7c6cb7d94f1c0b7302
tjhoning <tjhoning431@gmail.com>
2025-04-17 09:51:25 +0200

    pls werk

--- Assignment/run_me.py
+++ Assignment/run_me.py
@@ -98,34 +98,73 @@ while simulating == True:
             elif row["type"] == "gate":
                 gates_xy.append((row["x_pos"],row["y_pos"]))
 
-        #Specify node ids of gates, departure runways and arrival runways in a dict
-        start_and_goal_locations = {"gates": gates_xy, 
-                                    "dep_rwy": rwy_dep_xy,
-                                    "arr_rwy": rwy_arr_xy}
-        
-        #Create edges_dict from df_edges
-        edges_dict = {}
-        for i, row in df_edges.iterrows():
-            edge_id = (row["from"],row["to"])
-            from_node =  edge_id[0]
-            to_node = edge_id[1]
-            start_end_pos = (nodes_dict[from_node]["xy_pos"], nodes_dict[to_node]["xy_pos"])
-            edge_properties = {"id": edge_id,
-                            "from": row["from"],
-                            "to": row["to"],
-                            "length": row["length"],
-                            "weight": row["length"],
-                            "start_end_pos": start_end_pos
-                            }
-            edges_dict[edge_id] = edge_properties
+    #Specify node ids of gates, departure runways and arrival runways in a dict
+    start_and_goal_locations = {"gates": gates_xy, 
+                                "dep_rwy": rwy_dep_xy,
+                                "arr_rwy": rwy_arr_xy}
     
-        #Add neighbor nodes to nodes_dict based on edges between nodes
-        for edge in edges_dict:
-            from_node = edge[0]
-            to_node = edge[1]
-            nodes_dict[from_node]["neighbors"].add(to_node)  
-        
-        return nodes_dict, edges_dict, start_and_goal_locations
+    #Create edges_dict from df_edges
+    edges_dict = {}
+    for i, row in df_edges.iterrows():
+        edge_id = (row["from"],row["to"])
+        from_node =  edge_id[0]
+        to_node = edge_id[1]
+        start_end_pos = (nodes_dict[from_node]["xy_pos"], nodes_dict[to_node]["xy_pos"])
+        edge_properties = {"id": edge_id,
+                        "from": row["from"],
+                        "to": row["to"],
+                        "length": row["length"],
+                        "weight": row["length"],
+                        "start_end_pos": start_end_pos
+                        }
+        edges_dict[edge_id] = edge_properties
+
+    #Add neighbor nodes to nodes_dict based on edges between nodes
+    for edge in edges_dict:
+        from_node = edge[0]
+        to_node = edge[1]
+        nodes_dict[from_node]["neighbors"].add(to_node)  
+    
+    return nodes_dict, edges_dict, start_and_goal_locations
+
+### Define global variables
+
+nodes_file = "nodes_v2.xlsx" #xlsx file with for each node: id, x_pos, y_pos, type
+edges_file = "edges_v2.xlsx" #xlsx file with for each edge: from  (node), to (node), length
+nodes_dict, edges_dict, start_and_goal_locations = import_layout(nodes_file, edges_file)
+
+# Create lists of different kind of nodes
+gates = [node for node in nodes_dict if nodes_dict[node]["type"] == "gate"]
+rwy_dep = [node for node in nodes_dict if nodes_dict[node]["type"] == "rwy_d"]
+rwy_arr = [node for node in nodes_dict if nodes_dict[node]["type"] == "rwy_a"]
+tug_gates = [node for node in nodes_dict if nodes_dict[node]["type"] == "taxiparking"]
+
+### Set up simulation tracker
+
+results_folder = "simulation_results"
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
+columns_results = ["agent_id", "start", "goal", "arrival_time", "departure_time", "waiting_time", "taxi_time","optimal taxi_time"]
+simulating = True
+Number_of_sims = 2 #This should be deleted and swithed to a coefficient of variation checker
+sim_no = 1
+t_max = 50
+
+while simulating == True:
+    sim_results = []
+
+    #Parameters that can be changed:
+    simulation_time = 300
+    planner = "Independent" #choose which planner to use (currently only Independent is implemented)
+
+    #Visualization (can also be changed)
+    plot_graph = False                      # show graph representation in NetworkX
+    visualization = True                    # pygame visualization
+    slow_factor = 1                         # 5 here means 5 times slower
+    visualization_speed = 0.1*slow_factor   # set at 0.1 as default
+
+
 
     def create_graph(nodes_dict, edges_dict, plot_graph = True):
         """
@@ -162,11 +201,10 @@ while simulating == True:
             
         return graph
 
-    #%% RUN SIMULATION
     # =============================================================================
     # 0. Initialization
     # =============================================================================
-    nodes_dict, edges_dict, start_and_goal_locations = import_layout(nodes_file, edges_file)
+        
     graph = create_graph(nodes_dict, edges_dict, plot_graph)
     heuristics = calc_heuristics(graph, nodes_dict)
 
@@ -177,30 +215,17 @@ while simulating == True:
     if visualization:
         map_properties = map_initialization(nodes_dict, edges_dict) #visualization properties
 
-    # create lists of different kind of nodes
-    gates = [node for node in nodes_dict if nodes_dict[node]["type"] == "gate"]
-    # rwy_dep = [node for node in nodes_dict if nodes_dict[node]["type"] == "rwy_d"]
-    rwy_dep = [95, 96]
-    rwy_dep = [node for node in nodes_dict if nodes_dict[node]["type"] == "rwy_d"]
-    # rwy_dep = [95, 96]
-    rwy_arr = [node for node in nodes_dict if nodes_dict[node]["type"] == "rwy_a"]
-    # rwy_arr = [101, 102]
-    # tug_gates = [7, 9, 16, 23, 107]
-    tug_gates = [node for node in nodes_dict if nodes_dict[node]["type"] == "taxiparking"]
-
     # =============================================================================
     # 1. While loop and visualization
     # =============================================================================
     
-    #Start of while loop    
     running=True
     escape_pressed = False
     time_end = simulation_time
     dt = 0.1 #should be factor of 0.5 (0.5/dt should be integer)
-    t= 0
-    i=0
+    t = 0
+    i = 0
     arrival_available, dep_available = True, True
-
 
     print("\nSimulation Started\n")
     while running:
@@ -236,132 +261,80 @@ while simulating == True:
         
         #Spawn aircraft for this timestep (use for example a random process)
         # ==== Random Spawning ====
-        spawning_time = 4
-        if (t-1) % spawning_time == 0 and (arrival_available is not False or dep_available is not False):
-            ac_type = random.choice(['A','D','D']) #randomly choose arrival or departure
-            if ac_type == 'D':
-                # gates = [node for node in nodes_dict if nodes_dict[node]["type"] == "gate"]
-                available_gates = gates.copy()
-                for ac in aircraft_lst:
-                    if ac.status == "holding" or ac.status == "pickup":
-                        if ac.start in available_gates:
-                            available_gates.remove(ac.start)
-                        elif ac.from_to[0] in available_gates:
-                            available_gates.remove(ac.from_to[0])
-                        elif ac.goal in available_gates:
-                            available_gates.remove(ac.goal)
-                    elif ac.status == "taxiing": 
-                        if ac.goal in available_gates:
-                            available_gates.remove(ac.goal)
-        
-                #print("Available gates for departure: ", available_gates)
-                if len(available_gates) > 0:
-                    i += 1
-                    dep_available = True
-                    spawn_gate = random.choice(available_gates)
-                    ac = Aircraft(i, 'D', spawn_gate, random.choice(rwy_dep), t, nodes_dict)
-                    ac.status = "holding"
-                    aircraft_lst.append(ac)
-                    agent_lst.append(ac)
-        
-                else:
-                    print("==No gates available for departure")
-                    dep_available = False
-        
-            else:
-                # rwy_arr = [101, 102]
-                rwy_arr = [37,38]
-                available_rwy = rwy_arr
-                for ac in aircraft_lst:
-                    if ac.status == "holding" or ac.status == "pickup": # and not (ac.status == "arrived" or ac.status == "taxiing"):
-                        if ac.start in available_rwy:
-                            available_rwy.remove(ac.start)
-                        elif ac.from_to[0] in available_rwy:
-                            available_rwy.remove(ac.from_to[0])
-                #print("Available runways for arrival: ", available_rwy)
-                if len(available_rwy) > 0:
-                    i += 1
-                    arrival_available = True
-                    spawn_rwy = random.choice(available_rwy)
-                    ac = Aircraft(i, 'A', spawn_rwy, random.choice(gates), t, nodes_dict)
-                    ac.status = "holding"
-                    aircraft_lst.append(ac)
-                    agent_lst.append(ac)
-                else:
-                    print("==No runways available for arrival")
-                    arrival_available = False
-        # #
-        #
-        # ==== Fixed Spawning ====
-        spawning_time = 40
-        # if (t-1) % spawning_time == 0:
-        #     # case 1 - 4 aircraft which touch in the bottom right corner
-        #     ac = Aircraft(1, 'A', 37,36,t, nodes_dict)
-        #     ac.status = "holding"
-        #     ac1 = Aircraft(2, 'D', 36,37,t, nodes_dict)
-        #     ac1.status = "holding"
-        #     ac2 = Aircraft(3, 'A', 38,98,t, nodes_dict)
-        #     ac2.status = "holding"
-        #     ac3 = Aircraft(4, 'D', 98,38,t, nodes_dict)
-        #     ac3.status = "holding"
-        #     aircraft_lst.append(ac)
-        #     agent_lst.append(ac)
-        #     aircraft_lst.append(ac1)
-        #     agent_lst.append(ac1)
-        #     aircraft_lst.append(ac2)
-        #     agent_lst.append(ac2)
-        #     aircraft_lst.append(ac3)
-        #     agent_lst.append(ac3)
+        random_spawning = True
+
+        if random_spawning:
+            spawning_time = 4
+            if (t-1) % spawning_time == 0 and (arrival_available is not False or dep_available is not False):
+                ac_type = random.choice(['A','D','D']) #randomly choose arrival or departure
+                if ac_type == 'D':
+                    available_gates = gates.copy()
+                    for ac in aircraft_lst:
+                        if ac.status == "holding" or ac.status == "pickup":
+                            if ac.start in available_gates:
+                                available_gates.remove(ac.start)
+                            elif ac.from_to[0] in available_gates:
+                                available_gates.remove(ac.from_to[0])
+                            elif ac.goal in available_gates:
+                                available_gates.remove(ac.goal)
+                        elif ac.status == "taxiing": 
+                            if ac.goal in available_gates:
+                                available_gates.remove(ac.goal)
             
-        # if (t-1) % spawning_time == 10:
-            # # case 1 expansion
-            # ac4 = Aircraft(5, 'A', 37,36,t, nodes_dict)
-            # ac4.status = "holding"
-            # ac5 = Aircraft(6, 'D', 36,37,t, nodes_dict)
-            # ac5.status = "holding"
-            # agent_lst.append(ac4)
-            # agent_lst.append(ac5)
-            # aircraft_lst.append(ac4)
-            # aircraft_lst.append(ac5)
+                    print("Available gates for departure: ", available_gates)
+                    if len(available_gates) > 0:
+                        i += 1
+                        dep_available = True
+                        spawn_gate = random.choice(available_gates)
+                        ac = Aircraft(i, 'D', spawn_gate, random.choice(rwy_dep), t, nodes_dict)
+                        ac.status = "holding"
+                        aircraft_lst.append(ac)
+                        agent_lst.append(ac)
+            
+                    else:
+                        print("==No gates available for departure")
+                        dep_available = False
+            
+                else:
+                    rwy_arr = [37,38]
+                    available_rwy = rwy_arr
+                    for ac in aircraft_lst:
+                        if ac.status == "holding" or ac.status == "pickup": # and not (ac.status == "arrived" or ac.status == "taxiing"):
+                            if ac.start in available_rwy:
+                                available_rwy.remove(ac.start)
+                            elif ac.from_to[0] in available_rwy:
+                                available_rwy.remove(ac.from_to[0])
+                    print("Available runways for arrival: ", available_rwy)
+                    if len(available_rwy) > 0:
+                        i += 1
+                        arrival_available = True
+                        spawn_rwy = random.choice(available_rwy)
+                        ac = Aircraft(i, 'A', spawn_rwy, random.choice(gates), t, nodes_dict)
+                        ac.status = "holding"
+                        aircraft_lst.append(ac)
+                        agent_lst.append(ac)
+                    else:
+                        print("==No runways available for arrival")
+                        arrival_available = False
+        
+        
+        # ==== Fixed Spawning ====
+        if not random_spawning:
+            spawning_time = 40
+            if (t-1) % spawning_time == 0:
+                # case 1 - 4 aircraft which touch in the bottom right corner
+                ac = Aircraft(1, 'A', 35,1,t, nodes_dict)
+                ac.status = "holding"
+                ac1 = Aircraft(2, 'D', 36,1,t, nodes_dict)
+                ac1.status = "holding"
+                aircraft_lst.append(ac)
+                agent_lst.append(ac)
+                aircraft_lst.append(ac1)
+                agent_lst.append(ac1)
 
-            # # case 2 - 4 aircraft which needs to cross diagonally
-            # ac = Aircraft(1, 'A', 37,34,t, nodes_dict)
-            # ac.status = "holding"
-            # ac1 = Aircraft(2, 'D', 38,97,t, nodes_dict)
-            # ac1.status = "holding"
-            # aircraft_lst.append(ac)
-            # agent_lst.append(ac)
-            # aircraft_lst.append(ac1)
-            # agent_lst.append(ac1)
-            # ac2 = Aircraft(3, 'A', 34,37,t, nodes_dict)
-            # ac2.status = "holding"
-            # ac3 = Aircraft(4, 'D', 97,38,t, nodes_dict)
-            # ac3.status = "holding"
-            # aircraft_lst.append(ac2)
-            # agent_lst.append(ac2)
-            # aircraft_lst.append(ac3)
-            # agent_lst.append(ac3)
 
-
-        # if (t-1) % spawning_time == 10:
-        #     # case to check if taxibot creates collision with parked (holding) taxibot
-        #     ac4 = Aircraft(5, 'D', 98,96,t, nodes_dict)
-        #     ac4.status = "holding"
-        #     ac5 = Aircraft(6, 'D', 37,95,t, nodes_dict)
-        #     ac5.status = "holding"
-        #     agent_lst.append(ac4)
-        #     agent_lst.append(ac5)
-        #     aircraft_lst.append(ac4)
-        #     aircraft_lst.append(ac5)
-
-
-        #     constraints = []
-        # if t == 25:
-        #     aircraft_lst.clear()
-            # this clears the aircraft list just for case 2
         
         # ==== Spawning the taxibots ====
-        # spawning_locations = [7, 9, 16, 23, 107]
         spawning_locations = tug_gates
         alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         if t == 0:
@@ -378,12 +351,9 @@ while simulating == True:
 
             if t % 0.5 == 0:
                 PriorityDetector(agent_lst, t, edges_dict, nodes_dict, heuristics)
-                run_independent_planner_tugs(tug_lst, nodes_dict, edges_dict, heuristics, t, agent_lst, [t, t+0.5, t+1., t+1.5], constraints=constraints)
+                run_independent_planner_tugs(tug_lst, nodes_dict, edges_dict, heuristics, t, agent_lst, [t, t+0.5, t+1], constraints=constraints)
                 run_independent_planner(aircraft_lst, nodes_dict, edges_dict, heuristics, t, constraints=constraints)
 
-            # for ac in aircraft_lst:
-            #     if ac.status == "planning":
-            #         run_independent_planner(aircraft_lst, nodes_dict, edges_dict, heuristics, t, constraints=constraints)
                 
 
         elif planner == "Prioritized":
